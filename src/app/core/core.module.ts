a9e406ef58609ac1a4b678caf4392f4d08e4b018
ram2010mechatronics <ram2010mechatronics@gmail.com>
2019-07-22 22:47:12 +0530

    changes

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,45 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from './toolbar/toolbar.component';
-import {MatButtonModule, MatIconModule, MatRadioModule, MatToolbarModule} from '@angular/material';
-import {CoreRoutingModule} from './core-routing.module';
+import { MatButtonModule, MatFormFieldModule, MatIconModule, MatRadioModule, MatToolbarModule, MatInputModule} from '@angular/material';
+import { CoreRoutingModule } from './core-routing.module';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { FullscreenComponent } from './fullscreen/fullscreen.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { SidemenuItemComponent } from './sidemenu-item/sidemenu-item.component';
+import { ToolbarNotificationComponent } from './toolbar-notification/toolbar-notification.component';
+import { UserMenuComponent } from './user-menu/user-menu.component';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
 
 @NgModule({
-  declarations: [ToolbarComponent],
+  declarations: [ToolbarComponent, SearchBarComponent, FullscreenComponent, SidebarComponent,
+    SidemenuComponent, SidemenuItemComponent, ToolbarNotificationComponent, UserMenuComponent],
   imports: [
     CommonModule,
     CoreRoutingModule,
     MatToolbarModule,
     MatIconModule,
     MatRadioModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FlexLayoutModule,
+    PerfectScrollbarModule
+  ],
+
+  providers: [
+    {
+      provide: PERFECT_SCROLLBAR_CONFIG,
+      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
+    }
   ]
 })
 export class CoreModule { }
--- src/app/core/toolbar/toolbar.component.ts
+++ src/app/core/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ToolbarHelpers} from './toolbar.helpers';
 
 @Component({
   selector: 'msa-toolbar',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
+  @Input() sidenav;
+  @Input() sidebar;
+  @Input() drawer;
+  @Input() matDrawerShow;
+
+  searchOpen = false;
+  toolbarHelpers = ToolbarHelpers;
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
